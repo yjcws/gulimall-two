313c4a786355918ea5f512833780c6b6a34023b6
yjcwd <1531153813@qq.com>
2023-05-29 15:50:30 +0800

    添加一个测试模块

---
+++ gulimall-model-unit-test/src/main/java/com/example/gulimallmodelunittest/GulimallModelUnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gulimallmodelunittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallModelUnitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallModelUnitTestApplication.class, args);
+    }
+
+}
---
+++ gulimall-model-unit-test/src/test/java/com/example/gulimallmodelunittest/GulimallModelUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gulimallmodelunittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallModelUnitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
