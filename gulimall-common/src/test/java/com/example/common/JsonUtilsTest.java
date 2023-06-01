e916398e2c3b2758c4febde7f085d5bda9eb60f4
yjcwd <1531153813@qq.com>
2023-06-01 20:37:17 +0800

    json转化工具类

---
+++ gulimall-common/src/test/java/com/example/common/JsonUtilsTest.java
@@ -0,0 +1,18 @@
+package com.example.common;
+
+import com.example.common.utils.JsonUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JsonUtilsTest {
+    @Autowired
+    private JsonUtils jsonUtils;
+
+    @Test
+    public void testGetStringValue() {
+
+    }
+
+}
