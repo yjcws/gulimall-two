deb1de60152418f613618978c170583fc1e87398
yangjiancheng <yangjiancheng@iristar.com.cn>
2024-03-16 23:02:55 +0800

    fix-配置商城首页域名访问，nginx动静分离

--- gulimall-product/src/main/java/com/example/product/service/CategoryService.java
+++ gulimall-product/src/main/java/com/example/product/service/CategoryService.java
@@ -3,6 +3,7 @@ package com.example.product.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.common.utils.PageUtils;
 import com.example.product.entity.CategoryEntity;
+import com.example.product.vo.Catelog2Vo;
 
 import java.util.List;
 import java.util.Map;
@@ -29,5 +30,9 @@ public interface CategoryService extends IService<CategoryEntity> {
      * @return
      */
     Long[] findCatelogPath(Long catelogId);
+
+    List<CategoryEntity> getLevel1Categorys();
+
+    Map<String, List<Catelog2Vo>> getCatalogJson();
 }
 
