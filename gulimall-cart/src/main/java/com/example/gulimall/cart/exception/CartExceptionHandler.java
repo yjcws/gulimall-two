3002348c16c7717b0c41c84998a4ce9be994985e
yangjiancheng <yangjiancheng@iristar.com.cn>
2024-03-23 09:24:16 +0800

    fix-购物车服务模块开发

---
+++ gulimall-cart/src/main/java/com/example/gulimall/cart/exception/CartExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.example.gulimall.cart.exception;
+
+/**
+ * @Description:
+ * @Author: yjc
+ * @createTime: 2020-07-11 21:10
+ **/
+public class CartExceptionHandler extends RuntimeException {
+}
---
+++ gulimall-cart/src/main/java/com/example/gulimall/cart/to/UserInfoTo.java
@@ -0,0 +1,23 @@
+package com.example.gulimall.cart.to;
+
+import lombok.Data;
+
+/**
+ * @Description:
+ * @Author: yjc
+ * @createTime: 2020-06-30 17:35
+ **/
+
+@Data
+public class UserInfoTo {
+
+    private Long userId;
+
+    private String userKey;
+
+    /**
+     * 是否临时用户
+     */
+    private Boolean tempUser = false;
+
+}
