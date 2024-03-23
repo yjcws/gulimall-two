c375b0edf66fcd3ca44525fc2846f170e4a4464d
yangjiancheng <yangjiancheng@iristar.com.cn>
2024-03-23 09:11:03 +0800

    fix-单点登陆，授权服务模块开发

---
+++ gulimall-auth-server/src/main/java/com/example/gulimall/auth/vo/SocialUser.java
@@ -0,0 +1,24 @@
+package com.example.gulimall.auth.vo;
+
+import lombok.Data;
+
+/**
+ * @Description: 社交用户信息
+ * @Author: yjc
+ * @createTime: 2020-06-28 11:04
+ **/
+
+@Data
+public class SocialUser {
+
+    private String access_token;
+
+    private String remind_in;
+
+    private long expires_in;
+
+    private String uid;
+
+    private String isRealName;
+
+}
---
+++ gulimall-auth-server/src/main/java/com/example/gulimall/auth/vo/UserLoginVo.java
@@ -0,0 +1,17 @@
+package com.example.gulimall.auth.vo;
+
+import lombok.Data;
+
+/**
+ * @Description:
+ * @Author: yjc
+ * @createTime: 2020-06-27 19:07
+ **/
+
+@Data
+public class UserLoginVo {
+
+    private String loginacct;
+
+    private String password;
+}
